d96af6b6fab07dfb1d4e70b0040ad0b3c9c2f8e6
Sarur Santoyo Gonzalez <ant.45tr@gmail.com>
2024-12-06 00:04:43 -0600

    fak

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { registerUsers, singin } from "./controllers/UserController";
+import { createQuizz, getMetrics, getQuestionnaires } from "./controllers/QuestionnairesControllers";
 
 
 const app: Application = express();
@@ -13,9 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (_req: Request, res: Response) => {
     res.send("Hola desde mi servidor con TS");
 })
- 
 
-app.post("/users/create", registerUsers);
-app.post("users/singin", singin)
+//Usuarios
+app.post("/users/create", registerUsers)
+app.post("/users/sign-in", singin)
+
+app.post("/questionnaire/create", createQuizz)
+app.get("/questionnaire/get-metrics", getMetrics)
+app.get("/questionnaires/get-all", getQuestionnaires)
 
 export default app;
